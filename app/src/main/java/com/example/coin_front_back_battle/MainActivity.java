b0e0ca7c3e48bba8e00e2b8472b39608ee30b48a
DongMin-0604 <dongmin3147@gmail.com>
2021-12-20 20:45:19 +0900

    Initial commit

---
+++ app/src/main/java/com/example/coin_front_back_battle/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.coin_front_back_battle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
